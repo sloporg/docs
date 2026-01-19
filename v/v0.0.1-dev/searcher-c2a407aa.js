81e2d7b607005fa66b82d911f5e33688ea78617d
0xktn <0xktn@users.noreply.github.com>
2026-01-19 09:14:04 +0000

    deploy: e4af3005fe3a10e6acfba14368ca0e6f8d7e9e4b

--- v/v0.0.1-dev/searcher-c2a407aa.js
+++ v/v0.0.1-dev/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c659bc3a.js',
+                path_to_root + 'searchindex-c3de7fd5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
